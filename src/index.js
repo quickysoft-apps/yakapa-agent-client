0bf4caaba184f7e56b328c43da37333b07ddaa3c
neurosoup <neurosoup@gmail.com>
2017-11-15 08:15:11 +0000

    removed definition files and express dep

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-import App from './app'
 import Client from './client'
 
-const listen = (port) => {
-	const app = new App({
-		port
-	})
-	app.listen()
-	return app
-}
-
-const client = (server, tag, nickname) => {
-	return new Client({
-		tag,
-		nickname,
-		server
-	})
-}
-
 export default {
-	listen,
-	client
+	Client
 }
