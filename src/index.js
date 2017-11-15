9c3584734e5b97bddd1d318a744263a0f705349c
neurosoup <neurosoup@gmail.com>
2017-11-15 07:36:30 +0000

    Dissociate express app from agent client

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import App from './app'
 import Client from './client'
 
-export default class Agent {
+const listen = (port) => {
+	const app = new App({
+		port
+	})
+	app.listen()
+	return app
+}
 
-	constructor(options) {
-		
-		this._app = new App({
-			port: options.port
-		})		
-		this._app.listen()
-		
-		this._client = new Client({
-			tag: options.tag,
-			nickname: options.nickname,
-			server: options.server
-		})
-		
-	}
-	
-	get client() {
-    return this._client
-  }
+const client = (server, tag, nickname) => {
+	return new Client({
+		tag,
+		nickname,
+		server
+	})
+}
 
+export default {
+	listen,
+	client
 }
